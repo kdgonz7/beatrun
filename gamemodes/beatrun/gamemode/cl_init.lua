67f0e5199818e467f62f26f0c8251eba744ffb58
Kai D. Gonzalez <gkai70263@gmail.com>
2024-07-16 19:28:48 -0400

    rename beatrun - sandbox-beatrun

--- gamemodes/beatrun/gamemode/cl_init.lua
+++
@@ -1,5 +0,0 @@
-include("shared.lua")
-
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/cl/*.lua", "GAME")) do
-	include("cl/" .. v)
-end
--- gamemodes/sandbox-beatrun/gamemode/shared.lua
+++ gamemodes/sandbox-beatrun/gamemode/shared.lua
@@ -9,7 +9,7 @@ GM.Website = ""
 
 include("player_class/player_beatrun.lua")
 
-for _, v in ipairs(file.Find("gamemodes/beatrun/gamemode/sh/*.lua", "GAME", "nameasc")) do
+for _, v in ipairs(file.Find("gamemodes/sandbox-beatrun/gamemode/sh/*.lua", "GAME", "nameasc")) do
 	AddCSLuaFile("sh/" .. v)
 	include("sh/" .. v)
 end
